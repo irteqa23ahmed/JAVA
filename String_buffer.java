f05eae16af31c9445bb17760a2b381ffc5ba12c9
Mumtaz Irteqa Ahmed <41585977+irteqa23ahmed@users.noreply.github.com>
2019-09-07 12:48:52 +0530

    Add files via upload

---
+++ String_buffer.java
@@ -0,0 +1,11 @@
+public class String_buffer {
+public static void main(String args[]) {
+StringBuffer str = new StringBuffer("Hello WOrld!"); 
+System.out.println("str = "+str);)
+str.insert(5, "for"); 
+System.out.println("str.insert(5,\"for\") = "+str);
+str.reverse();
+System.out.println("str.reverse() = "+str);
+str.replace(5, 8, "are");
+System.out.println("str.replace(5, 8, \"are\"); = "+str);
+str.
