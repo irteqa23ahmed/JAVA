6868b0d7600bd6a644d3191b114afe73b72a14e3
Mumtaz Irteqa Ahmed <41585977+irteqa23ahmed@users.noreply.github.com>
2019-09-07 14:27:07 +0530

    Update Move_Zero_to_end.java

--- Move_Zero_to_end.java
+++ Move_Zero_to_end.java
@@ -1,9 +1,14 @@
 public class Move_Zero_to_end {
 	public static void main(String args[]) {
-	int arr[] = {4,3,2,0,1,0,9,0,78,0,0,12,34,0,0,1,0,8,7,6,0,4,3,2};
+	int arr[] = new arr[50];
 	int n,i,j;
 	int temp;
-	n = arr.length;	
+	Scanner scan = new Scanner(System.in);
+        System.out.println("Enter size of integer array: ");
+        n = scan.nextInt();
+        System.out.println("Enter integer array : ");
+        for(int i=0;i<n;i++) 
+		arr[i]=scan.nextInt();
 	j=n-1;
 	for(i=0;i<j;i++)
 	{
