1fb44a0f6c91fa43bf139eb4535dc23d4d269f7c
Mumtaz Irteqa Ahmed <41585977+irteqa23ahmed@users.noreply.github.com>
2019-09-07 14:35:16 +0530

    Update Move_Zero_to_end.java

--- Move_Zero_to_end.java
+++ Move_Zero_to_end.java
@@ -1,27 +1,23 @@
+import java.util.*;
 public class Move_Zero_to_end {
 	public static void main(String args[]) {
-	int arr[] = new arr[50];
-	int n,i,j;
-	int temp;
+	int arr[] = new int[50];
+	int a[] = new int[50];
+	int n,i,j=0;
+	int count=0;
 	Scanner scan = new Scanner(System.in);
-        System.out.println("Enter size of integer array: ");
-        n = scan.nextInt();
-        System.out.println("Enter integer array : ");
-        for(int i=0;i<n;i++) 
-		arr[i]=scan.nextInt();
-	j=n-1;
-	for(i=0;i<j;i++)
+    System.out.println("Enter size of integer array: ");
+    n = scan.nextInt();
+    System.out.println("Enter integer array : ");
+    for(i=0;i<n;i++) 
 	{
-        if(arr[i] == 0)
-        {
-	for(;arr[j]==0;j--);
-        temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
+	    arr[i]=scan.nextInt();
+	    if(arr[i]==0) count++;
+	    else a[j++]=arr[i];
 	}
-	}
-	for(i=0;i<n;i++){
-	System.out.println(arr[i]);
-	}
+	for(i=0;i<count;i++)
+	    a[j++]=0;
+	for(i=0;i<n;i++)
+	System.out.println(a[i]);
 	}
 	}
