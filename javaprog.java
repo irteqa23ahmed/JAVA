18bf3867b6557bb3d88c6cad5519e03724dd5467
Mumtaz Irteqa Ahmed <41585977+irteqa23ahmed@users.noreply.github.com>
2019-10-13 23:13:00 +0530

    Create javaprog.java

---
+++ javaprog.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+class Data 
+{
+    public boolean flag = true;
+    synchronized public void display_java() 
+    {
+        while(true)
+        {   
+                while (!flag) 
+                {
+                    try 
+                    { 
+                        wait();
+                    }
+                    catch (Exception e) {}
+                }
+                flag = false;
+                System.out.print("JAVA");
+                notify();
+            }
+    }
+    synchronized public void display_programming() 
+    {
+        while(true){   
+                while(flag) 
+                {
+                    try 
+                    { 
+                        wait();
+                    }
+                    catch (Exception e) {} 
+                }
+                flag = true;
+                System.out.println(" PROGRAMMING");
+                notify();
+        }
+    }
+}
+class jThread extends Thread
+{   
+    Data obj1;
+    jThread(Data obj)
+    {
+        obj1=obj;
+    }
+    public void run()
+    {
+        obj1.display_java();
+    }
+}
+class pThread extends Thread
+{   
+    Data obj1;
+    pThread(Data obj)
+    {
+        obj1=obj;
+    }
+    public void run()
+    {
+        obj1.display_programming();
+    }
+} 
+
+public class javaprog {
+    public static void main(String args[])
+    {
+	Data obj=new Data();
+        jThread t1 =new jThread(obj);
+        pThread t2 =new pThread(obj);
+        t1.start();
+        t2.start();
+    }
+}
